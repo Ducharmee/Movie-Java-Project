e41589cfd7b082209476940a1224890a8d67bd0c
parkerj12 <parkerj12@WIT45001537.wit.private>
2019-03-31 23:35:36 -0400

    update 3

--- src/JavaFx.java
+++ src/JavaFx.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.URL;
 
 import java.util.ResourceBundle;
@@ -5,9 +6,12 @@ import java.util.ResourceBundle;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -20,18 +24,34 @@ public class JavaFx extends Application implements Initializable{
 	
 	@FXML
 	ChoiceBox myChoice;
+	@FXML
+	Stage stage;
+	Stage stage2;
+	Scene scene1;
+	Scene scene2;
 	
+	FXMLLoader loader;
+	static String[] primaryStage;
 	public static void main(String[] args) {
 		
 		launch(args);
 	}
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		final FXMLLoader loader = new FXMLLoader(getClass().getResource("moviefxml.fxml"));
-		final Pane p = loader.load();
+		stage = primaryStage; 
+		Pane p = null;
 		
-		primaryStage.setScene(new Scene(p));
-		primaryStage.show();
+		loader = new FXMLLoader(getClass().getResource("moviefxml.fxml"));
+		p = loader.load();
+		
+
+	
+		
+		stage.setScene(new Scene(p));
+		
+		
+		stage.show();
+		stage = primaryStage; 
 		
 	}
 	@Override
@@ -42,6 +62,33 @@ public class JavaFx extends Application implements Initializable{
 		
 		// TODO Auto-generated method stub
 		
+	}
+	
+	//First Button onAction Method
+	public void button1Click(ActionEvent event) throws IOException{
+		Theatre t = new Theatre();
+		t.type = myChoice.getSelectionModel().getSelectedItem().toString();
+		
+		Pane p = null;
+		
+		loader = new FXMLLoader(getClass().getResource("new.fxml"));
+		p = loader.load();
+		
+		//Parent tableview = FXMLLoader.load(getClass().getResource("new.fxml"));
+		//Scene tableviewScene = new Scene(tableview);
+		
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+		window.setScene(new Scene(p));
+		window.show();
+	//	loader = new FXMLLoader(getClass().getResource("locations.fxml"));
+	//	Pane p = loader.load();
+		
+		//stage.setScene(new Scene(p));
+	//	stage.show();
+		
+	
+			
+	
 	}
 
 }
